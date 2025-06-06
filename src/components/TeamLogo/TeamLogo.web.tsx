ce449234902334a5f709fcff97a7f4f46dfe1cdd
Luka <lukams03b@gmail.com>
2025-06-06 16:28:39 -0400

    fix(web): flatten team logo styles

--- src/components/TeamLogo/TeamLogo.web.tsx
+++ src/components/TeamLogo/TeamLogo.web.tsx
@@ -1,17 +1,21 @@
 // components/TeamLogo/TeamLogo.web.tsx
 import React from 'react';
+import { ImageStyle, StyleProp, StyleSheet } from 'react-native';
 
 interface TeamLogoProps {
   uri: string;
-  style?: React.CSSProperties;
+  style?: StyleProp<ImageStyle>;
 }
 
 const TeamLogo = ({ uri, style }: TeamLogoProps) => {
+  // StyleSheet.flatten handles numbers and arrays from StyleSheet.create
+  const flatStyle = StyleSheet.flatten(style) as React.CSSProperties;
+
   return (
     <img
       src={uri}
       alt="Team Logo"
-      style={{ width: 40, height: 40, objectFit: 'contain', ...style }}
+      style={{ width: 40, height: 40, objectFit: 'contain', ...flatStyle }}
     />
   );
 };
